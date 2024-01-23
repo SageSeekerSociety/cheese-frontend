1552776524b75365e4fe862d4cf7bbc458aab721
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2024-01-23 23:47:05 +0800

    feat(user): sign in

--- src/network/Interceptors/hooks/forbidden.ts
+++ src/network/Interceptors/hooks/forbidden.ts
@@ -1,15 +1,13 @@
 import { messageFailed } from '../../utils/showMessage';
-import { Session } from '../../utils/storage';
+import { Local } from '../../utils/storage';
+import { useRouter } from 'vue-router';
 
-/*
+const router = useRouter();
 
-如使用react-router / vue-route请将 `window.location.replace('/login')` 推荐替换对应Api方法
-
-*/
 export default function forbidden() {
   // 根据业务逻辑，执行对应处理
   // 需求：账号权限发生改变，触发403 退出到登录页
   messageFailed('账号权限改变，请重新登录');
-  Session.clear();
-  window.location.replace('/login');
+  Local.clear();
+  router.replace('/account/signin');
 }
