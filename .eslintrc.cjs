a40e578a9ebf2af2b04ee37c28b9fb3017652c99
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2024-01-24 15:04:24 +0800

    chore: run linter and prettier on commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
+    es2021: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+    "plugin:prettier/recommended",
     "prettier",
   ],
   rules: {
     "vue/multi-word-component-names": "off",
   },
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
 };
