493f07b88b05f349d981db48d98b22b8507009d2
ce_amtic <43514587+ce-amtic@users.noreply.github.com>
2024-02-21 14:38:30 +0800

    feat: users (#8)
    * feat: user api
    * fix: typo
    * feat: profile editor
    
    ---------
    
    Co-authored-by: HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>

--- src/layouts/user/User.vue
+++ src/layouts/user/User.vue
@@ -1,26 +1,95 @@
 <template>
   <app-bar :links="links" />
-  <v-main class="bg-grey-lighten-3">
-    <user-card :user-data="fakeUserData" />
+  <v-main v-if="loaded" class="bg-grey-lighten-3">
+    <user-card :key="userData.id" :profile="userData" />
     <v-container class="d-flex row">
       <v-row>
         <v-col class="pr-0">
           <v-sheet rounded="lg">
-            <v-tabs v-model="selectedTab">
-              <v-tab v-for="tab in tabs" :key="tab.label" :to="tab.route" exact>
-                {{ tab.label }}
-              </v-tab>
-            </v-tabs>
             <router-view />
           </v-sheet>
         </v-col>
         <v-col cols="3">
-          <v-sheet rounded="lg">
-            <v-list rounded="lg">
-              <v-list-item v-for="n in 5" :key="n" link :title="`List Item ${n}`"></v-list-item>
-              <v-divider class="my-2"></v-divider>
-              <v-list-item color="grey-lighten-4" link title="Refresh"></v-list-item>
-            </v-list>
+          <v-sheet class="pt-3 pb-3 rounded-lg">
+            <v-tabs v-model="selectedTab" direction="vertical">
+              <v-tab v-for="tab in tabs" :key="tab.label" :to="tab.route" exact>
+                {{ tab.label }}
+              </v-tab>
+            </v-tabs>
+
+            <div v-if="isActiveUser()">
+              <v-divider class="my-2" />
+              <v-dialog width="800">
+                <template #activator="{ props }">
+                  <v-btn v-bind="props" color="primary" variant="text" block @click="resetDefault">
+                    <v-icon left>mdi-cog</v-icon>
+                    编辑资料
+                  </v-btn>
+                </template>
+                <template #default="{ isActive }">
+                  <v-card title="修改信息">
+                    <form class="pt-2 pl-5 pr-5" @submit.prevent="submit">
+                      <v-row>
+                        <v-col cols="8">
+                          <v-list-subheader inset>昵称</v-list-subheader>
+                          <v-text-field
+                            v-model="nickname.value.value"
+                            :counter="20"
+                            :error-messages="nickname.errorMessage.value"
+                          ></v-text-field>
+
+                          <v-list-subheader inset>个人简介</v-list-subheader>
+                          <v-text-field
+                            v-model="intro.value.value"
+                            :counter="300"
+                            :error-messages="intro.errorMessage.value"
+                          ></v-text-field>
+
+                          <v-list-subheader inset>头像</v-list-subheader>
+                          <v-file-input
+                            v-model="selectedFile"
+                            accept="image/*"
+                            prepend-icon="mdi-plus"
+                            label="选择文件"
+                            chips
+                            show-size
+                            show-size-hint
+                            outlined
+                            @change="handleFileChange"
+                          />
+                        </v-col>
+                        <v-col cols="4">
+                          <v-list-subheader inset>预览</v-list-subheader>
+                          <v-img
+                            v-if="selectedFile.length > 0"
+                            :src="previewUrl"
+                            aspect-ratio="1"
+                            class="rounded-lg"
+                            rounded="0"
+                            size="180"
+                          />
+                          <v-img
+                            v-else
+                            :src="userData.avatar"
+                            color="grey-darken-1"
+                            aspect-ratio="1"
+                            class="rounded-lg"
+                            rounded="0"
+                            size="180"
+                          />
+                        </v-col>
+                      </v-row>
+                    </form>
+                    <v-card-actions>
+                      <v-btn @click="submit"> 提交 </v-btn>
+                      <v-btn @click="resetDefault"> 重置 </v-btn>
+                      <v-spacer />
+                      <v-btn @click="isActive.value = false"> 关闭 </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </template>
+              </v-dialog>
+            </div>
           </v-sheet>
         </v-col>
       </v-row>
@@ -29,9 +98,94 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, onMounted, watch, computed, toRefs } from 'vue'
+import { useRoute } from 'vue-router'
+import { UserApi } from '@/network/api/user'
+import { ImageApi } from '@/network/api/image'
+import { User } from '@/types/users'
+import { useField, useForm } from 'vee-validate'
+import AccountService from '@/services/account'
 import AppBar from '@/components/common/AppBar/AppBar.vue'
 import UserCard from '@/components/user/UserCard.vue'
+
+const selectedTab = ref(0)
+const route = useRoute()
+const userID = computed(() => parseInt(route.params.id[0]))
+const userData = ref({} as User)
+const loaded = ref(false)
+
+const isActiveUser = computed(() => {
+  return () => parseInt(route.params.id[0]) === AccountService._user.value?.id
+})
+
+onMounted(() => {
+  fetchData().then((result) => {
+    userData.value = result.data as User
+    loaded.value = true
+    // console.log(AccountService._loggedIn.value);
+  })
+})
+watch(
+  // 切换用户时刷新
+  () => route.params.id,
+  () => {
+    window.location.reload()
+  }
+)
+function fetchData() {
+  const result = UserApi.getUserInfo(userID.value)
+  return result
+}
+
+const { handleSubmit, handleReset } = useForm({
+  validationSchema: {
+    nickname(value: string) {
+      if (value?.length >= 4 && value.length <= 20) return true
+      return '昵称的长度需要在4-20个字符之间。'
+    },
+    intro(value: string) {
+      if (value?.length <= 300) return true
+      return '个人简介不能超过300个字符。'
+    },
+    avatar(value: string) {
+      return true
+    },
+  },
+})
+const nickname = useField('nickname')
+const intro = useField('intro')
+const submit = handleSubmit((values) => {
+  uploadAvatar()
+  values.avatar = 'default.jpg'
+  alert(JSON.stringify(values, null, 2))
+})
+const selectedFile = ref([])
+const previewUrl = ref('')
+const resetDefault = () => {
+  nickname.value.value = userData.value.nickname
+  intro.value.value = userData.value.intro
+  selectedFile.value = []
+}
+const handleFileChange = () => {
+  if (selectedFile.value) {
+    readAvatar()
+  } else {
+    previewUrl.value = ''
+  }
+}
+const readAvatar = () => {
+  const reader = new FileReader()
+  reader.onload = (event) => {
+    previewUrl.value = event.target?.result as string
+  }
+  reader.readAsDataURL(selectedFile.value[0] as Blob)
+}
+const uploadAvatar = () => {
+  const formData = new FormData()
+  formData.append('file', selectedFile.value[0])
+  const result = ImageApi.upload(formData)
+  return result
+}
 
 const links = [
   {
@@ -51,19 +205,34 @@ const links = [
   },
 ]
 
-const fakeUserData = {
-  username: 'Administrator',
-  userDesc: '芝士雪豹，一言以蔽之',
-  // userAvatar: 'https://pica.zhimg.com/v2-c9f27d4b3a6e090de02f71925f8d8f50_l.jpg?source=32738c0c',
-  userAvatar: 'https://gravatar.com/userimage/132994463/e7e95572d023b832c6e91517084c6968.jpeg?size=256',
-  userHeaderImg: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
-  // userHeaderImg: 'https://s2.loli.net/2023/04/18/QTFneJBOH68Njlg.jpg',
-}
-
-const selectedTab = ref(0)
-
 const tabs = [
-  { label: '提问', route: '/user' },
-  { label: '回答', route: '/user/answer' },
+  {
+    label: '提问',
+    route: {
+      name: 'UserQuestion',
+      params: { id: route.params.id },
+    },
+  },
+  {
+    label: '回答',
+    route: {
+      name: 'UserAnswer',
+      params: { id: route.params.id },
+    },
+  },
+  {
+    label: '关注',
+    route: {
+      name: 'UserFollowing',
+      params: { id: route.params.id },
+    },
+  },
+  {
+    label: '粉丝',
+    route: {
+      name: 'UserFollower',
+      params: { id: route.params.id },
+    },
+  },
 ]
 </script>
--- src/router/user.ts
+++ src/router/user.ts
@@ -1,22 +1,32 @@
 export default {
-  path: '/user',
+  path: '/user/:id',
   name: 'User',
   component: () => import('@/layouts/user/User.vue'),
   children: [
     {
       path: '',
-      name: 'UserDefault', // ParentName->ParentNameDefault 这个关键字敏感
-      component: () => import('@/views/user/Question.vue'),
-    },
-    {
-      path: 'question',
       name: 'UserQuestion',
       component: () => import('@/views/user/Question.vue'),
     },
+    // {
+    //   path: 'question',
+    //   name: 'UserQuestion',
+    //   component: () => import('@/views/user/Question.vue'),
+    // },
     {
       path: 'answer',
       name: 'UserAnswer',
       component: () => import('@/views/user/Answer.vue'),
+    },
+    {
+      path: 'following',
+      name: 'UserFollowing',
+      component: () => import('@/views/user/Following.vue'),
+    },
+    {
+      path: 'follower',
+      name: 'UserFollower',
+      component: () => import('@/views/user/Follower.vue'),
     },
   ],
 }
