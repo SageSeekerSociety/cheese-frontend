83e42c11fc4cc842c4449f4c1218eb2154017711
arielherself <ariel@arielherself.xyz>
2024-02-22 14:05:06 +0800

    feat(insights): create APIs

--- src/types/insight.ts
+++ src/types/insight.ts
@@ -1,6 +1,30 @@
 export type Insight = {
   id: number
-  userid: number
-  title: string | null
   content: string
+  author: {
+    id: number
+    username: string
+    nickname: string
+    avatar: string
+    intro: string
+  }
+  created_at: number
+  updated_at: number
+  is_like: boolean
+  like_count: number
+  comment_count: number
+  view_count: number
+  medias: InsightMedia[]
+}
+
+export type InsightMedia = {
+  id: number
+  type: string
+  url: string
+  meta: {
+    width: number
+    height: number
+    size: number
+    thumbnail: string
+  }
 }
