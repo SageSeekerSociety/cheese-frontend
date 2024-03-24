5183c303204d8eae45dadbcb4454edf10ba28414
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2024-03-24 17:02:30 +0800

    feat(questions): topics

--- src/views/question/Ask.vue
+++ src/views/question/Ask.vue
@@ -2,23 +2,26 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-sheet rounded="lg" class="pa-1">
-          <v-text-field
-            v-model="title"
-            label="问题标题"
-            hide-details="auto"
-            variant="plain"
-            class="mx-4 question-title-input"
-            :single-line="true"
-            required
-          ></v-text-field>
-          <rich-editor holder="editor" :config="DEFAULT_CONFIG" @create="onCreate" />
+        <v-sheet rounded="lg" class="pa-4">
+          <v-form>
+            <v-text-field
+              v-model="title"
+              label="问题标题"
+              variant="plain"
+              class="question-title-input"
+              hide-details="auto"
+              :single-line="true"
+              v-bind="titleProps"
+            ></v-text-field>
+            <rich-editor holder="editor" :config="DEFAULT_CONFIG" @create="onCreate" />
+            <topic-selector v-model="topics" class="mt-4" v-bind="topicsProps" />
+          </v-form>
         </v-sheet>
       </v-col>
 
       <v-col cols="3">
         <v-sheet rounded="lg" class="pa-2 mb-4"> 提问的艺术 </v-sheet>
-        <v-btn block flat rounded="lg" @click="submit"> 发布问题 </v-btn>
+        <v-btn block flat rounded="lg" :loading="isSubmitting" @click="submit"> 发布问题 </v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -26,28 +29,63 @@
 
 <script setup lang="ts">
 import RichEditor from '@/components/common/Editor/Editor.vue'
-import { ref } from 'vue'
 import EditorJS from '@editorjs/editorjs'
 import { QuestionApi } from '@/network/api/questions'
 import { useRouter } from 'vue-router'
 import { DEFAULT_CONFIG } from '@/utils/editor'
 import { toast } from 'vuetify-sonner'
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import { z } from 'zod'
+import { vuetifyConfig } from '@/utils/form'
+import TopicSelector from '@/components/questions/TopicSelector.vue'
 
 let editor: EditorJS
-const title = ref('')
 const router = useRouter()
+
+const { handleSubmit, defineField, isSubmitting } = useForm({
+  validationSchema: toTypedSchema(
+    z.object({
+      title: z
+        .string()
+        .trim()
+        .refine((v) => v.length > 0, {
+          message: '问题标题不能为空',
+        })
+        .refine((v) => v.endsWith('?') || v.endsWith('？'), {
+          message: '问题标题必须以问号结尾',
+        }),
+      topics: z
+        .object({
+          id: z.number(),
+          name: z.string(),
+        })
+        .array()
+        .refine((v) => v.length > 0, {
+          message: '至少选择一个话题',
+        }),
+    })
+  ),
+})
+const [title, titleProps] = defineField('title', vuetifyConfig)
+const [topics, topicsProps] = defineField('topics', vuetifyConfig)
 
 const onCreate = (editorInstance: EditorJS) => {
   editor = editorInstance
 }
 
-const submit = async () => {
+const submit = handleSubmit(async (values) => {
   const outputData = await editor.save()
   const content = JSON.stringify(outputData)
-  const res = await QuestionApi.ask({ title: title.value, content, type: 0, topics: [] })
+  const res = await QuestionApi.ask({
+    title: values.title,
+    content,
+    type: 0,
+    topics: values.topics.map((topic) => topic.id),
+  })
   toast.success('提问成功')
   router.push({ name: 'QuestionAnswerList', params: { questionId: res.data.id } })
-}
+})
 </script>
 
 <style>
@@ -57,7 +95,7 @@ const submit = async () => {
 }
 
 .ce-block__content {
-  padding: 0 16px;
+  padding: 0;
 }
 
 .question-title-input {
@@ -66,5 +104,13 @@ const submit = async () => {
 .question-title-input .v-field__input,
 .question-title-input .v-field-label {
   font-size: 1.5rem;
+}
+
+.question-topics {
+  gap: 8px;
+}
+
+.question-topic-input {
+  width: 240px;
 }
 </style>
