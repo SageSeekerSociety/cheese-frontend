e67ea5ef3f5046364044c8f70481b54d59991f9d
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2024-03-31 02:22:12 +0800

    refactor(users): questions

--- src/views/user/Question.vue
+++ src/views/user/Question.vue
@@ -1,48 +1,32 @@
 <template>
-  <v-card v-for="item in questionList" :key="item.title" flat class="pt-2 pb-2 rounded-lg pl-2 pr-2">
-    <v-card-title
-      ><span class="text-h6 font-weight-bold">{{ item.title }}</span></v-card-title
-    >
-    <v-card-subtitle>{{ item.content }}</v-card-subtitle>
-    <v-card-actions>
-      <v-btn variant="plain" color="on-background">
-        <v-icon size="18" class="me-2">mdi-message-text</v-icon>
-        {{ item.answer_count }}回答
-      </v-btn>
-      <v-btn variant="plain" color="on-background">
-        <v-icon size="18" class="me-2">mdi-star-outline</v-icon>
-        {{ item.follow_count }}收藏
-      </v-btn>
-      <v-btn variant="plain" color="on-background" min-width="32px">
-        <v-icon size="18">mdi-dots-horizontal</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <template v-if="refreshing">
+    <v-skeleton-loader v-for="i in 5" :key="i" type="list-item-avatar, paragraph, button@2" />
+  </template>
+  <template v-else>
+    <question-card v-for="question in questionList" :key="question.id" :data="question" />
+  </template>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
+import { onMounted, computed } from 'vue'
 import { UserApi } from '@/network/api/users'
 import { useRoute } from 'vue-router'
-import { Question } from '@/types'
+import QuestionCard from '@/components/questions/QuestionCard.vue'
+import { usePaging } from '@/utils/paging'
 
 const route = useRoute()
 const userID = computed(() => parseInt(route.params.id[0], 10))
-const questionList = ref<Question[]>([])
-const loaded = ref(false)
 
-onMounted(() => {
-  fetchData().then(({ data: { questions } }) => {
-    questionList.value = questions
-    loaded.value = true
-  })
+const {
+  data: questionList,
+  refresh,
+  refreshing,
+} = usePaging(async (pageStart) => {
+  const {
+    data: { questions: data, page },
+  } = await UserApi.getQuestionList(userID.value, pageStart)
+  return { data, page }
 })
 
-function fetchData() {
-  const result = UserApi.getQuestionList(userID.value, {
-    pageStart: 0,
-    pageSize: 10,
-  })
-  return result
-}
+onMounted(refresh)
 </script>
