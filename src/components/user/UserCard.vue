c580a89493598460ad5a197336b5b8770d55ce8b
ce-amtic <fyh516@126.com>
2024-04-08 15:07:57 +0800

    feat: added avatar upload and crop functions

--- src/components/user/UserCard.vue
+++ src/components/user/UserCard.vue
@@ -10,7 +10,13 @@
         cover
       />
       <v-container class="cut-align-center">
-        <user-avatar :has-avatar="hasAvatar" :avatar="profile.avatar" :size="120" :class="'rounded-lg'" :rounded="0" />
+        <user-avatar
+          :has-avatar="true"
+          :avatar="getUrlByAvatarId(profile.avatarId)"
+          :size="120"
+          :class="'rounded-lg'"
+          :rounded="0"
+        />
         <v-container class="info-wrapper">
           <v-row>
             <v-col class="text-h4 font-weight-bold me-4">
@@ -30,7 +36,7 @@
                 </v-btn>
               </div>
               <div v-else>
-                <v-dialog width="800">
+                <v-dialog v-model="isEditDialogVisible" width="800">
                   <template #activator="{ props }">
                     <v-btn v-bind="props" color="primary" variant="flat" @click="handleReset">
                       <v-icon left>mdi-cog</v-icon>
@@ -40,44 +46,72 @@
                   <template #default="{ isActive }">
                     <v-card title="修改信息">
                       <form class="pt-2 pl-5 pr-5" @submit.prevent="submit">
-                        <v-row>
-                          <v-col cols="8">
+                        <div class="d-flex flex-row justify-space-between">
+                          <div class="flex-grow-1 me-5">
                             <v-list-subheader inset>昵称</v-list-subheader>
-                            <v-text-field v-model="selectedNickname" v-bind="nicknameProps"></v-text-field>
+                            <v-text-field
+                              ref="nicknameField"
+                              v-model="selectedNickname"
+                              v-bind="nicknameProps"
+                            ></v-text-field>
                             <v-list-subheader inset>个人简介</v-list-subheader>
                             <v-text-field v-model="selectedIntro" :counter="60" v-bind="introProps"></v-text-field>
-
-                            <v-list-subheader inset>头像</v-list-subheader>
-                            <v-file-input
-                              v-model="selectedAvatar"
-                              accept="image/*"
-                              prepend-icon="mdi-plus"
-                              label="选择文件"
-                              chips
-                              show-size
-                              outlined
-                              v-bind="avatarProps"
-                              @change="handleFileChange"
-                            />
-                          </v-col>
-                          <v-col cols="4">
-                            <v-list-subheader inset>预览</v-list-subheader>
-                            <v-img
-                              :src="previewUrl"
-                              aspect-ratio="1"
-                              class="rounded-lg"
-                              rounded="0"
-                              size="180"
-                              color="grey-darken-1"
-                            />
-                          </v-col>
-                        </v-row>
+                          </div>
+                          <div>
+                            <v-list-subheader inset>头像预览</v-list-subheader>
+                            <div class="position-relative" style="width: 175px; height: 175px">
+                              <div
+                                class="overlay rounded-lg"
+                                @click="avatarInput.click()"
+                                @mouseover="overlay = true"
+                                @mouseleave="overlay = false"
+                              >
+                                <v-icon v-show="overlay" class="position-absolute" color="white">mdi-upload</v-icon>
+                              </div>
+                              <user-avatar
+                                v-if="croppedUrl == ''"
+                                :has-avatar="true"
+                                :avatar="getUrlByAvatarId(profile.avatarId)"
+                                :size="175"
+                                :class="'rounded-lg'"
+                                :rounded="0"
+                              />
+                              <img v-else :src="croppedUrl" style="width: 175px; height: 175px" class="rounded-lg" />
+                              <input
+                                ref="avatarInput"
+                                accept="image/*"
+                                type="file"
+                                style="display: none"
+                                @change="previewAvatar"
+                              />
+                            </div>
+                          </div>
+                        </div>
                       </form>
                       <v-card-actions>
                         <v-btn @click="submit"> 提交 </v-btn>
-                        <v-btn @click="handleReset"> 重置 </v-btn>
+                        <v-btn @click="reset"> 重置 </v-btn>
                         <v-spacer />
                         <v-btn @click="isActive.value = false"> 关闭 </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </template>
+                </v-dialog>
+                <v-dialog v-model="isCropperDialogVisible" max-width="450">
+                  <template #default="{ isActive }">
+                    <v-card>
+                      <cropper
+                        ref="avatarCropper"
+                        class="cropper"
+                        :src="previewUrl"
+                        :stencil-props="{
+                          aspectRatio: 1,
+                        }"
+                      />
+                      <v-card-actions>
+                        <v-btn @click="cropAvatar"> 确认 </v-btn>
+                        <v-spacer />
+                        <v-btn @click="isActive.value = false"> 取消 </v-btn>
                       </v-card-actions>
                     </v-card>
                   </template>
@@ -125,20 +159,24 @@
 
 <script lang="ts" setup>
 import { ref, onMounted, computed, inject, watch, Ref } from 'vue'
+import { toast } from 'vuetify-sonner'
 import { toTypedSchema } from '@vee-validate/zod'
-import { z } from 'zod'
+import { number, z } from 'zod'
 import { User } from '@/types/users'
 import { UserApi } from '@/network/api/users'
-import { ImageApi } from '@/network/api/image'
+import { AvatarApi } from '@/network/api/avatars'
 import { useForm } from 'vee-validate'
 import { vuetifyConfig } from '@/utils/form'
+import { Cropper } from 'vue-advanced-cropper'
+import { getUrlByAvatarId } from '@/utils/user'
 import AccountService from '@/services/account'
 import UserAvatar from '../common/UserAvatar.vue'
+import 'vue-advanced-cropper/dist/style.css'
+import { getErrorMessage } from '@/utils/errors'
 
 const profile = inject<Ref<User>>('userData', ref({} as User))
-
-const hasAvatar = computed(() => {
-  return !!profile?.value.avatar && profile?.value.avatar != 'default.jpg' && profile?.value.avatar != 'deafult.jpg'
+const userRefresh = inject<Function>('userRefresh', () => {
+  toast.error('userRefresh not provided.')
 })
 
 const myId = computed(() => AccountService._user.value?.id as number)
@@ -148,6 +186,16 @@ const isActivatedUser = computed(() => {
 
 const isFollowLoading = ref(false)
 const isFollowing = ref(true)
+
+const previewUrl = ref('')
+const croppedUrl = ref('')
+const croppedBlob = ref({} as Blob)
+const overlay = ref(false)
+const isCropperDialogVisible = ref(false)
+const isEditDialogVisible = ref(false)
+const avatarInput = ref(null)
+const avatarCropper = ref(null)
+const nicknameField = ref(null)
 
 const followUser = async () => {
   const result = await UserApi.followUser(profile?.value.id as number)
@@ -179,7 +227,7 @@ const { handleSubmit, defineField, handleReset, resetForm } = useForm({
       nickname: z.string().min(4).max(16),
       intro: z.string().max(60),
       avatar: z
-        .array(z.instanceof(File).refine((v) => v.size < 2 * 1024 * 1024, { message: '文件大小不能超过 2MB' }))
+        .array(z.instanceof(File).refine((v) => v.size < 1024 * 1024, { message: '文件大小不能超过 2MB' }))
         .optional(),
     })
   ),
@@ -197,48 +245,83 @@ watch(
         intro: newVal.intro,
       },
     })
+    croppedUrl.value = ''
   },
   { immediate: true }
 )
 
 const [selectedNickname, nicknameProps] = defineField('nickname', vuetifyConfig)
 const [selectedIntro, introProps] = defineField('intro', vuetifyConfig)
-const [selectedAvatar, avatarProps] = defineField('avatar', vuetifyConfig)
-const previewUrl = ref('')
-const handleFileChange = () => {
-  previewUrl.value = ''
-  const length = selectedAvatar.value?.length ?? 0
-  if (length > 0) {
-    readAvatar()
-  }
+// const [selectedAvatar, avatarProps] = defineField('avatar', vuetifyConfig)
+
+const reset = () => {
+  croppedUrl.value = ''
+  if (avatarInput.value) avatarInput.value.value = ''
+  handleReset()
 }
-const readAvatar = () => {
-  if (!selectedAvatar.value) return
-  if (selectedAvatar.value.length === 0) return
-  const file = selectedAvatar.value[0]
+
+const previewAvatar = (event: Event) => {
+  const fileInput = event.target as HTMLInputElement
+  const file = fileInput.files?.[0]
+
+  if (!file) {
+    return
+  }
+
   const reader = new FileReader()
+  reader.onload = (e) => {
+    const target = e.target as FileReader
+    previewUrl.value = target.result as string
+    isCropperDialogVisible.value = true
+  }
   reader.readAsDataURL(file)
-  reader.onload = (event) => {
-    previewUrl.value = event.target?.result as string
+}
+
+const cropAvatar = () => {
+  if (avatarCropper.value) {
+    croppedUrl.value = avatarCropper.value.getResult().canvas.toDataURL()
+    avatarCropper.value.getResult().canvas.toBlob((blob: Blob) => {
+      croppedBlob.value = blob
+    })
+    isCropperDialogVisible.value = false
   }
 }
-const uploadAvatar = () => {
-  if (!selectedAvatar.value) return
-  if (selectedAvatar.value.length === 0) return
-  const file = selectedAvatar.value[0]
+
+watch(isCropperDialogVisible, () => {
+  if (isCropperDialogVisible.value == false) {
+    nicknameField.value?.focus()
+  }
+})
+
+const uploadAvatar = async () => {
   const formData = new FormData()
-  formData.append('file', file)
-  const result = ImageApi.upload(formData)
-  return result
+  const blob = croppedBlob.value
+  formData.append('avatar', blob)
+  try {
+    const { data: result } = await AvatarApi.uploadAvatar(formData)
+    return result.avatarid
+  } catch (e) {
+    toast.error('头像大小不能超过1MB，请重新调整')
+  }
+  return -1
 }
-const submit = handleSubmit((values) => {
-  // uploadAvatar()
+
+const submit = handleSubmit(async (values) => {
+  const avatarId = croppedUrl.value == '' ? profile.value.avatarId : await uploadAvatar()
+  if (avatarId == -1) return
   const submitData = {
     nickname: values.nickname,
     intro: values.intro,
-    avatar: 'default.jpg',
+    avatarId: avatarId,
   }
-  alert(JSON.stringify(submitData, null, 2))
+  try {
+    await UserApi.updateUserInfo(profile.value.id, submitData)
+  } catch (e) {
+    toast.error(getErrorMessage(e))
+  }
+  toast.success('用户信息更新成功')
+  isEditDialogVisible.value = false
+  userRefresh()
 })
 </script>
 
@@ -263,5 +346,20 @@ const submit = handleSubmit((values) => {
   padding-right: 0;
   padding-top: 0;
   padding-bottom: 0;
+}
+.overlay {
+  position: absolute;
+  z-index: 10;
+  width: 175px;
+  height: 175px;
+  background-color: rgba(0, 0, 0, 0);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color 0.3s;
+}
+
+.overlay:hover {
+  background-color: rgba(0, 0, 0, 0.5);
 }
 </style>
